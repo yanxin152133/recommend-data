3bb85cac42bbbde2fce5e582aa3021f216115228
yanxin152133 <yanxin152133@gmail.com>
2020-05-16 23:50:01 +0800

    豆瓣读书爬虫程序

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # recommend-data
 爬取豆瓣读书书籍信息
+
+## 入库
+编辑**pipelines.py**文件：
+           
+```html
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import pymysql as db
+
+
+class RecommenddataPipeline(object):
+    def __init__(self):
+        self.con = db.connect(user="root", passwd="123456+", host="localhost",
+                              db="recommend", charset="utf8")
+        self.cur = self.con.cursor()
+
+    def process_item(self, item, spider):
+        book_name = item.get("book_name", "N/A")  # 有的图书有数据项缺失，这里做了容错处理
+        book_author = item.get("book_author", "N/A")
+        book_publish = item.get("book_publish", "N/A")
+        book_date = item.get("book_date", "N/A")
+        book_price = item.get("book_price", "N/A")
+        book_star = item.get("book_star", "N/A")
+        book_img = item.get("book_img", "N/A")
+        book_content = item.get("book_content", "N/A")
+        book_type = item.get("book_type", "N/A")
+
+        self.cur.execute(
+            "insert into douban_books(id,book_name,book_author,book_publish,book_date,book_price,book_star,book_img,book_content,book_type) values(NULL,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+            (item['book_name'], item['book_author'], item['book_publish'], item['book_date'], item['book_price'],
+             item['book_star'], item['book_img'], item['book_content'], item['book_type']))
+        self.con.commit()
+        return item
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.conn.close()
+
+```
+
+## 测试
+```html
+cd recommendData
+scrapy crawl bookspider -o items.json  ## 将爬取的数据写入items.json文件中
+```
+
+## 运行
+```html
+scrapy crawl bookspider
+```
--- recommendData/spiders/bookspider.py
+++ recommendData/spiders/bookspider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import os
 from scrapy.selector import Selector
 
 from recommendData.items import BooksItem
@@ -8,20 +9,46 @@ from recommendData.items import BooksItem
 class BookspiderSpider(scrapy.Spider):
     name = 'bookspider'
     allowed_domains = ['book.douban.com']
-    start_urls = ['https://book.douban.com/tag/%E4%B8%AD%E5%9B%BD%E6%96%87%E5%AD%A6']
+    start_urls = ['https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4',
+                  'https://book.douban.com/tag/%E4%B8%AD%E5%9B%BD%E6%96%87%E5%AD%A6',
+                  'https://book.douban.com/tag/%E5%8F%A4%E5%85%B8%E6%96%87%E5%AD%A6',
+                  'https://book.douban.com/tag/%E5%AD%A6%E6%9C%AF',
+                  'https://book.douban.com/tag/%E6%8E%A8%E7%90%86',
+                  'https://book.douban.com/tag/%E7%BB%98%E6%9C%AC',
+                  'https://book.douban.com/tag/%E7%A7%91%E5%B9%BB',
+                  'https://book.douban.com/tag/%E9%87%91%E5%BA%B8',
+                  'https://book.douban.com/tag/%E5%8E%86%E5%8F%B2',
+                  'https://book.douban.com/tag/%E5%BF%83%E7%90%86%E5%AD%A6',
+                  'https://book.douban.com/tag/%E4%BC%A0%E8%AE%B0',
+                  'https://book.douban.com/tag/%E5%9B%9E%E5%BF%86%E5%BD%95',
+                  'https://book.douban.com/tag/%E6%80%9D%E6%83%B3',
+                  'https://book.douban.com/tag/%E7%88%B1%E6%83%85',
+                  'https://book.douban.com/tag/%E6%88%90%E9%95%BF',
+                  'https://book.douban.com/tag/%E7%94%9F%E6%B4%BB',
+                  'https://book.douban.com/tag/%E6%97%85%E8%A1%8C',
+                  'https://book.douban.com/tag/%E7%BB%8F%E6%B5%8E%E5%AD%A6',
+                  'https://book.douban.com/tag/%E7%BB%8F%E6%B5%8E%E5%AD%A6',
+                  'https://book.douban.com/tag/%E7%A7%91%E6%99%AE',
+                  'https://book.douban.com/tag/%E7%BC%96%E7%A8%8B',
+                  'https://book.douban.com/tag/%E7%A7%91%E5%AD%A6',
+                  'https://book.douban.com/tag/%E7%A7%91%E6%8A%80',
+                  'https://book.douban.com/tag/%E6%B8%B8%E8%AE%B0',
+                  'https://book.douban.com/tag/%E7%BE%8E%E9%A3%9F',
+                  'https://book.douban.com/tag/%E5%85%BB%E7%94%9F',
+                  'https://book.douban.com/tag/%E5%88%9B%E4%B8%9A'
+                  ]
 
     def parse(self, response):
         sel = Selector(response)
         book_list = sel.css('#subject_list >ul>li')
         book_type = sel.css('#content')
-
+        count = 0  # 用于设置爬取的页数
         for book in book_list:
             item = BooksItem()
             try:
                 item['book_type'] = book_type.xpath('h1/text()').extract()[0].strip()
                 item['book_name'] = book.xpath('div[@class="info"]/h2/a/text()').extract()[0].strip()
-                item['book_star'] = book.xpath("div[@class='info']/div[2]/span[@class='rating_nums']/text()").extract()[
-                    0].strip()
+                item['book_star'] = book.xpath("div[@class='info']/div[2]/span[@class='rating_nums']/text()").extract()[0].strip()
                 pub = book.xpath('div[@class="info"]/div[@class="pub"]/text()').extract()[0].strip().split('/')
                 item['book_price'] = pub.pop()
                 item['book_date'] = pub.pop()
@@ -32,8 +59,10 @@ class BookspiderSpider(scrapy.Spider):
                 yield item
             except:
                 pass
-            nextPage = sel.xpath('//div[@id="subject_list"]/div[@class="paginator"]/span[@class="next"]/a/@href').extract()[
-                0].strip()
-            if nextPage:
-                    next_url = 'https://book.douban.com' + nextPage
-                    yield scrapy.http.Request(next_url, callback=self.parse)
+        nextPage = sel.xpath('//div[@id="subject_list"]/div[@class="paginator"]/span[@class="next"]/a/@href').extract()[0].strip()
+        if nextPage:
+            next_url = 'https://book.douban.com' + nextPage
+            count += 1
+            yield scrapy.http.Request(next_url, callback=self.parse)
+        if count > 50:
+            os.exit();
